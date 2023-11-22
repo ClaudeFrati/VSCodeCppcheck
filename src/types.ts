2ccfdbd2a831794f22499a533725d0a25ca445a1
Aaron Levine <ald.levine@gmail.com>
2023-11-22 13:36:37 -0500

    flesh out project file schema

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,26 @@
+type TagDir = {
+    name: "string"
+}
+
+type TagDirList = {
+    dir?: TagDir | TagDir[]
+}
+
+// TODO: figure out suppressions and other key settings!
+// Basic description found at: https://github.com/danmar/cppcheck/blob/main/gui/projectfile.txt
+// Consider using a different, less opinionated, xml parser to remain true to file structure
 export type CppcheckProjectFile = {[key: string]: any} & {
-    root?: {
-        name: string
+    root?: TagDir
+    builddir?: string
+    platform?: string
+    paths?: TagDirList
+    includedir?: TagDirList
+    exclude?: TagDirList
+    libraries?: {
+        library?: string | string[]
+    }
+    addons?: {
+        addon?: string | string[]
     }
 }
 
